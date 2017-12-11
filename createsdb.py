5d4caf5c29354b796d3f8a49122eb1f2afb6272d
Amanda Bacon <amandabacon@bennington.edu>
2017-12-10 22:37:05 -0500

    working..

--- createsdb.py
+++ createsdb.py
@@ -7,7 +7,6 @@
 import time
 import psycopg2
 import psycopg2.extras
-import csv
 
 # Create db-- sudo -u postgres createdb -O amandabacon stickney_db
 
@@ -16,7 +15,7 @@ def create_table():
 		connection = psycopg2.connect("dbname = 'stickney_db' user = 'amandabacon'")
 		cursor = connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
 		cursor.execute("""DROP TABLE IF EXISTS SObSet""")
-		cursor.execute("""CREATE TABLE SObSet (id NUMERIC NOT NULL PRIMARY KEY, item_name VARCHAR(50) NOT NULL, model VARCHAR(50) NOT NULL, serial_num VARCHAR(25) NOT NULL, date TIMESTAMP, logger_name VARCHAR(20) NULL, description TEXT NULL);""")
+		cursor.execute("""CREATE TABLE SObSet (id SERIAL NOT NULL PRIMARY KEY, item_name VARCHAR(50) NOT NULL, model VARCHAR(50) NOT NULL, serial_num VARCHAR(25) NOT NULL, date TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(), logger_name VARCHAR(20) NULL, description TEXT NULL);""")
 
 		try:
 			cursor.execute("""SELECT tables.table_schema, table_name FROM information_schema.tables WHERE table_name = 'sobset' ORDER BY table_schema, table_name;""")
@@ -27,9 +26,13 @@ def create_table():
 			print("Error in fetching table: {0}".format(e.message))
 
 		try:
-			cursor.execute("INSERT INTO SObSet (id, item_name, model, serial_num, date, logger_name, description) VALUES(1,'Test','Test','Test','2017-11-29 00:00:00','A','Test')")
-			cursor.execute("INSERT INTO SObSet (id, item_name, model, serial_num, date, logger_name, description) VALUES(2,'TEST','TEST','TEST','2017-12-03 02:00:00','A','TEST')")
-                	cursor.execute("INSERT INTO SObSet (id, item_name, model, serial_num, date, logger_name, description) VALUES(3,'TEST','TEST','TEST','2017/12/03 02:10:20','A','TEST')")
+			cursor.execute("INSERT INTO SObSet (item_name, model, serial_num, logger_name, description) VALUES('Test','Test','Test','A','Test')")
+			cursor.execute("INSERT INTO SObSet (item_name, model, serial_num, logger_name, description) VALUES('TEST','TEST','TEST','A','TEST')")
+                	cursor.execute("INSERT INTO SObSet (item_name, model, serial_num, logger_name, description) VALUES('TEST','TEST','TEST','A','TEST')")
+                        cursor.execute("INSERT INTO SObSet (item_name, model, serial_num, logger_name, description) VALUES('Meade','TEST','TEST','A','TEST')")
+                        cursor.execute("INSERT INTO SObSet (item_name, model, serial_num, logger_name, description) VALUES('Meade Test','TEST','TEST','A','TEST')")
+                        cursor.execute("INSERT INTO SObSet (item_name, model, serial_num, logger_name, description) VALUES('MEADE','TEST','TEST','A','TEST')")
+                        cursor.execute("INSERT INTO SObSet (item_name, model, serial_num, logger_name, description) VALUES('MEADE','TEST','TEST','A','TEST')")
 			print("Insertion complete")
 
 		except:
@@ -50,7 +53,50 @@ def create_table():
 	except Exception as e:
 		print("Unable to connect to database: {0}".format(e.message))
 
+def user_table():
+        try:
+                connection = psycopg2.connect("dbname = 'stickney_db' user = 'amandabacon'")
+                cursor = connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+                cursor.execute("""DROP TABLE IF EXISTS user_table""")
+                cursor.execute("""CREATE TABLE user_table (id SERIAL NOT NULL PRIMARY KEY, user_name VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, email VARCHAR(25) NOT NULL, privilege INT NOT NULL DEFAULT 0);""")
+
+                try:
+                        cursor.execute("""SELECT tables.table_schema, table_name FROM information_schema.tables WHERE table_name = 'user_table' ORDER BY table_schema, table_name;""")
+                        for table in cursor.fetchall():
+                                print(table)
+
+                except Exception as e:
+                        print("Error in fetching table: {0}".format(e.message))
+
+                try:
+                        cursor.execute("INSERT INTO user_table (user_name, password, email) VALUES('Test','Test','Test')")
+                        print("Insertion complete")
+
+                except:
+                        print("Cannot insert into table")
+
+                try:
+                        cursor.execute("""SELECT * FROM user_table""")
+                        inserts = cursor.fetchall()
+                        print(inserts)
+
+                except:
+                        print("Error: Cannot select from table")
+
+                connection.commit()
+                cursor.close()
+                connection.close()
+
+        except Exception as e:
+                print("Unable to connect to database: {0}".format(e.message))
+
 if __name__ == '__main__':
 	create_table()
+	user_table()
 
 print(time.time(), time.clock())
+
+# Create trigger function on date to update row when item updated (to view type \df or \df+)
+# CREATE OR REPLACE FUNCTION update_date() RETURNS TRIGGER AS $$ BEGIN NEW.date = now(); RETURN NEW; END; $$ language 'plpgsql';
+# CREATE TRIGGER update_sobset_date BEFORE UPDATE ON sobset FOR EACH ROW EXECUTE PROCEDURE update_date();
+# found here: https://stackoverflow.com/questions/1035980/update-timestamp-when-row-is-updated-in-postgresql
--- so.py
+++ so.py
@@ -4,41 +4,146 @@
 	Author: Amanda Bacon (amandabacon@bennington.edu)
 	Date: 2017/11/27
 """
-import time
 import web
+import hashlib
 
 render = web.template.render('templates/')
 
 web.config.debug = False
 
 urls = (
+	'/login', 'login',
 	'/objectinfo/(.*)', 'info',
-	'/object_by_name/(.*)', 'object'
-
+	'/object_by_name', 'objects',
+	'/logout', 'logout',
+	'/formsub', 'form'
 )
 
 app = web.application(urls, globals(), True)
+store = web.session.DiskStore('sessions')
+sessions = web.session.Session(app, store, initializer = {'login': 0, 'privilege': 0})
+
+def logged():
+	if session.login == 1:
+		return True
+	else:
+		return False
+
+def create_render(privilege):
+	if logged():
+		if privilege == 0:
+			render = web.template.render('templates/reader')
+		elif privilege == 1:
+			render = web.template.render('templates/user')
+		elif privilege == 2:
+			render = web.template.render('templates/admin')
+		else:
+			render = web.template.render('templates/communs')
+	else:
+		render = web.template.render('templates/')
+	return render
+
+class login:
+	def GET(self):
+		if logged():
+			render = create_render(session.get('privilege'))
+			return '%s' % render.login_double()
+		else:
+			render = create_render(session.get('privilege'))
+			return '%s' % render.login()
+
+	def POST(self):
+                db = web.database(dbn = 'postgres', user = 'amandabacon', db = 'stickney_db')
+		user_name, password = web.input().user_name, web.input().password
+		ident = db.select('user_table', where = "user_name=$user_name='{0}'".format(ident))
+		try:
+			if hashlib.shal("sAlT754-"+password).hexdigest() == ident['pass']:
+				session.login = 1
+				session.privilege = ident['privilege']
+				render = create_render(session.get('privilege'))
+				return render.login_ok()
+			else:
+				session.login = 0
+				session.privilege = 0
+				render = create_render(session.get('privilege'))
+				return render.login_error()
+		except:
+			session.login = 0
+			session.privilege = 0
+			render = create_render(session.get('privilege'))
+			return render.login_error()
+
+class logout:
+	def GET(self):
+		session.login = 0
+		session.kill()
+		render = create_render(session.get('privilege'))
+		return render.logout()
 
 class info:
 	def GET(self,id):
-		db = web.database(dbn = 'postgres', user='amandabacon', db = 'stickney_db')
-		info = db.select('object', where = "id='{0}'".format(id))
-		return render.stickneyinfo(info)
+		db = web.database(dbn = 'postgres', user = 'amandabacon', db = 'stickney_db')
+		info = db.select('sobset', where = "id='{0}'".format(id))
+		return render.objectinfo(info)
 		if not id:
 			id = 'no id'
 		return None
 
 class objects:
 	def POST(self):
-		db = web.database(dbn = 'postgres', user='amandabacon', db = 'stickney_db')
-		name = web.input().name
-		objects = db.select('object', where = "name='{0}'".format(name))
-		return render.name(objects)
+		db = web.database(dbn = 'postgres', user = 'amandabacon', db = 'stickney_db')
+		item_name = web.input().item_name
+		objects = db.select('sobset', where = "item_name='{0}'".format(item_name))
+		return render.sobset(objects)
 		item = web.input()
-		item1 = item.name
-		print item1
+		item1 = item.item_name
+		print(item1)
 		return item1
 
+class form:
+	def POST(self):
+		db = web.database(dbn = 'postgres', user = 'amandabacon', db = 'stickney_db')
+		try:
+			item_name = web.input().item_name
+			model = web.input().model
+			serial_num = web.input().serial_num
+			logger_name = web.input().logger_name
+			description = web.input().description
+			form = db.insert('sobset', item_name = item_name, model = model, serial_num = serial_num, logger_name = logger_name, description = description)
+			return render.formsub(form)
+			first = web.input()
+			first1 = first.item_name
+			print(first1)
+			return first1
+
+			return render.formsub(form)
+			second = web.input()
+			second2 = second.model
+			print(second2)
+			return second2
+
+			return render.formsub(form)
+			third = web.input()
+			third3 = third.serial_num
+			print(third3)
+			return third3
+
+			return render.formsub(form)
+			fourth = web.input()
+			fourth4 = fourth.logger_name
+			print(fourth4)
+			return fourth4
+		
+			return render.formsub(form)
+			fifth = web.input()
+			fifth5 = fifth.description
+			print(fifth5)
+			return fifth5
+
+			form.commit()
+
+		except Exception as e:
+			print("Error in inserting into table: {0}".format(e.message))
 
 if __name__ == "__main__":
 	app.run()
