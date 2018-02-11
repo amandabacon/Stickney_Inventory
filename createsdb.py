d575f338b68c69b4184605d5f078f680bf732cd2
Amanda Bacon <amandabacon@bennington.edu>
2018-02-11 14:46:08 -0500

    tidy up code

--- createsdb.py
+++ createsdb.py
@@ -26,13 +26,7 @@ def create_table():
 			print("Error in fetching table: {0}".format(e.message))
 
 		try:
-			cursor.execute("INSERT INTO SObSet (item_name, model, serial_num, logger_name, description) VALUES('Test','Test','Test','A','Test')")
-			cursor.execute("INSERT INTO SObSet (item_name, model, serial_num, logger_name, description) VALUES('TEST','TEST','TEST','A','TEST')")
-                	cursor.execute("INSERT INTO SObSet (item_name, model, serial_num, logger_name, description) VALUES('TEST','TEST','TEST','A','TEST')")
-                        cursor.execute("INSERT INTO SObSet (item_name, model, serial_num, logger_name, description) VALUES('Meade','TEST','TEST','A','TEST')")
-                        cursor.execute("INSERT INTO SObSet (item_name, model, serial_num, logger_name, description) VALUES('Meade Test','TEST','TEST','A','TEST')")
-                        cursor.execute("INSERT INTO SObSet (item_name, model, serial_num, logger_name, description) VALUES('MEADE','TEST','TEST','A','TEST')")
-                        cursor.execute("INSERT INTO SObSet (item_name, model, serial_num, logger_name, description) VALUES('MEADE','TEST','TEST','A','TEST')")
+			cursor.execute("INSERT INTO SObSet (item_name, model, serial_num, logger_name, description) VALUES('Test','Test','Test','AJ','Test')")
 			print("Insertion complete")
 
 		except:
@@ -54,41 +48,41 @@ def create_table():
 		print("Unable to connect to database: {0}".format(e.message))
 
 def user_table():
-        try:
-                connection = psycopg2.connect("dbname = 'stickney_db' user = 'amandabacon'")
-                cursor = connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
-                cursor.execute("""DROP TABLE IF EXISTS user_table""")
-                cursor.execute("""CREATE TABLE user_table (id SERIAL NOT NULL PRIMARY KEY, user_name VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, email VARCHAR(25) NOT NULL, privilege INT NOT NULL DEFAULT 0);""")
+	try:
+		connection = psycopg2.connect("dbname = 'stickney_db' user = 'amandabacon'")
+		cursor = connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+		cursor.execute("""DROP TABLE IF EXISTS user_table""")
+		cursor.execute("""CREATE TABLE user_table (id SERIAL NOT NULL PRIMARY KEY, user_name VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, email VARCHAR(25) NOT NULL, privilege INT NOT NULL DEFAULT 0);""")
 
-                try:
-                        cursor.execute("""SELECT tables.table_schema, table_name FROM information_schema.tables WHERE table_name = 'user_table' ORDER BY table_schema, table_name;""")
-                        for table in cursor.fetchall():
-                                print(table)
+		try:
+			cursor.execute("""SELECT tables.table_schema, table_name FROM information_schema.tables WHERE table_name = 'user_table' ORDER BY table_schema, table_name;""")
+			for table in cursor.fetchall():
+				print(table)
 
-                except Exception as e:
-                        print("Error in fetching table: {0}".format(e.message))
+		except Exception as e:
+			print("Error in fetching table: {0}".format(e.message))
 
-                try:
-                        cursor.execute("INSERT INTO user_table (user_name, password, email) VALUES('Test','Test','Test')")
-                        print("Insertion complete")
+		try:
+			cursor.execute("INSERT INTO user_table (user_name, password, email) VALUES('Test','Test','Test')")
+			print("Insertion complete")
 
-                except:
-                        print("Cannot insert into table")
+		except:
+			print("Cannot insert into table")
 
-                try:
-                        cursor.execute("""SELECT * FROM user_table""")
-                        inserts = cursor.fetchall()
-                        print(inserts)
+		try:
+			cursor.execute("""SELECT * FROM user_table""")
+			inserts = cursor.fetchall()
+			print(inserts)
 
-                except:
-                        print("Error: Cannot select from table")
+		except:
+			print("Error: Cannot select from table")
 
-                connection.commit()
-                cursor.close()
-                connection.close()
+		connection.commit()
+		cursor.close()
+		connection.close()
 
-        except Exception as e:
-                print("Unable to connect to database: {0}".format(e.message))
+	except Exception as e:
+		print("Unable to connect to database: {0}".format(e.message))
 
 if __name__ == '__main__':
 	create_table()
